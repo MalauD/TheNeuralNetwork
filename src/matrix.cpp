283ccb27dff255d5bc1860020bbefbd2ec8ebbc2
Malaury <malaurydutour@outlook.fr>
2019-08-17 17:55:13 +0200

    Matrix pt2

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -4,6 +4,8 @@ matrix::matrix(int16_t rows,int16_t cols){
     this->rows = rows;
     this->cols = cols;
     
+    srand(time(NULL));
+
     data = new double* [rows];
 
     for(int i = 0; i < rows; i++){
@@ -14,18 +16,62 @@ matrix::matrix(int16_t rows,int16_t cols){
     }
 }
 
-matrix matrix::fromArray(double arr[]){
+matrix::~matrix(){
+    for(int i = 0; i < rows; ++i) {
+    delete [] data[i];
+    }
+    delete [] data;
+}
 
+matrix* matrix::fromArray(std::vector<double> arr){
+    auto mT = new matrix(arr.size(),1);
+    mT->map([&ArrayR = arr](double val,int16_t rows,int16_t cols){
+        return ArrayR[rows];
+    });
+    return mT;
 }
 
 matrix matrix::randomize(matrix* m){
-
+    m->map([](double val,int16_t rows,int16_t cols){
+        return ((double) rand() / RAND_MAX) * 2 - 1; //! rnd number btw -1 and 1
+    });
 }
 
-matrix matrix::map(matrix* m,void* func){
-
+void matrix::map(std::function<double(double,int16_t,int16_t)> func){
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++){
+            double val = data[rows][cols];
+            *data[rows,cols] = func(val,rows,cols);
+        }
+    }
 }
 
 matrix matrix::transpose(matrix* m){
 
+}
+
+matrix matrix::operator+(const matrix & b){
+
+}
+
+matrix matrix::operator-(const matrix & b){
+    
+}
+
+matrix matrix::operator*(const matrix & b){
+    
+}
+
+matrix matrix::operator*(const double b){
+    
+}
+
+void matrix::print(){
+    std::cout << "==== " << rows << " x " << cols << " ====";
+    map([&c = cols](double tval,int16_t trows,int16_t tcols){
+        std::cout << tval << " ";
+        if(tcols == c)
+            std::cout << "\n";
+    });
+    std::cout << "\n";
 }
--- src/matrix.h
+++ src/matrix.h
@@ -1,5 +1,8 @@
 #include <stdint.h>
 #include <iostream>
+#include <vector>
+#include <functional>
+#include <time.h>
 
 class matrix {
     private:
@@ -7,13 +10,14 @@ class matrix {
         double **data;
     public:
         matrix(int16_t rows,int16_t cols);
-        static matrix fromArray(double arr[]);
+        ~matrix();
+        static matrix* fromArray(std::vector<double> arr);
         static matrix randomize(matrix* m);
-        static matrix map(matrix* m,void* func);
+        void map(std::function<double(double,int16_t,int16_t)> func);
         static matrix transpose(matrix* m);
         matrix operator+ (const matrix& b);
         matrix operator- (const matrix& b);
         matrix operator* (const matrix& b);
         matrix operator* (const double b);
-        friend std::ostream& operator<<(std::ostream &strm, const matrix &a);        
+        void print();
 };
